4a9a6f151edeebeb49dc60bae1d1f989a7338f1a
Stablest <marlonlocci@hotmail.com>
2023-06-15 11:14:03 -0300

    feature: Added not-found middleware to app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import { userRouter } from "./routers/users";
+import { notFound } from "./middlewares/not-found";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/v1/users", userRouter);
+
+app.use(notFound);
 
 export { app };
