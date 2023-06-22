18b1c8a3194388017c73498f712645e0950762a3
Stablest <marlonlocci@hotmail.com>
2023-06-22 09:57:11 -0300

    feat: Added IUserInstance interface, added permission key to IUser

--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -4,4 +4,10 @@ export interface IUser {
   name: String;
   email: String;
   password: String;
+  permission: number;
+}
+
+export interface IUserInstance extends IUser, Document {
+  createJWT: () => string;
+  comparePassword: (passwordReceived: string) => boolean;
 }
