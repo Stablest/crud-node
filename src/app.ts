ed4b5dab55e7b9ff0ea16e65942fad8e31b0674a
Stablest <marlonlocci@hotmail.com>
2023-06-22 09:51:22 -0300

    feat: Added errorHandler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import { userRouter } from "./routers/users";
 import { notFound } from "./middlewares/not-found";
+import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/v1/users", userRouter);
-
 app.use(notFound);
+app.use(errorHandler);
 
 export { app };
