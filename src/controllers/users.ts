39b80638ad4564cff3fab745607f9300d3c58d9b
Stablest <marlonlocci@hotmail.com>
2023-06-22 16:15:32 -0300

    refactor: Removed unused functions

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { userModel } from "../models/User";
 import { IUser } from "../interfaces/IUser";
+import { AuthenticationError } from "../errors/authentication-error";
 
 async function getAllUsers(req: express.Request, res: express.Response) {
   try {
@@ -11,48 +12,4 @@ async function getAllUsers(req: express.Request, res: express.Response) {
   }
 }
 
-async function createUser(
-  req: express.Request<any, any, IUser>,
-  res: express.Response,
-  next: express.NextFunction
-) {
-  try {
-    const newUser = await userModel.create({ ...req.body });
-    const newToken = newUser.createJWT();
-    res.status(201).json({ user: { id: newUser._id }, token: newToken });
-  } catch (err: unknown) {
-    next(err);
-  }
-}
-
-async function getUser(req: express.Request, res: express.Response) {
-  try {
-    const userFound = await userModel.find({ _id: req.params.id });
-    return res.status(200).json({ user: userFound });
-  } catch (e) {
-    return res.status(500).json({ error: e });
-  }
-}
-
-async function updateUser(req: express.Request, res: express.Response) {
-  try {
-    const updatedUser = await userModel.updateOne(
-      { _id: req.params.id },
-      { ...req.body }
-    );
-    return res.status(200).json({ user: updatedUser });
-  } catch (e) {
-    return res.status(400).json({ error: e });
-  }
-}
-
-async function deleteUser(req: express.Request, res: express.Response) {
-  try {
-    const deletedUser = await userModel.deleteOne({ _id: req.params.id });
-    return res.status(200).json({ user: deletedUser });
-  } catch (e) {
-    return res.status(400).json({ error: e });
-  }
-}
-
-export { getAllUsers, createUser, getUser, updateUser, deleteUser };
+export { getAllUsers };
