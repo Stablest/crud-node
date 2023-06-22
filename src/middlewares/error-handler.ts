1d09f24ef1540bda94ddf354d27c8785bba50d7c
Stablest <marlonlocci@hotmail.com>
2023-06-22 10:03:33 -0300

    feat: Added errorHandler middleware

---
+++ src/middlewares/error-handler.ts
@@ -0,0 +1,48 @@
+import express from "express";
+import { CustomAPIError } from "../errors/custom-api-error";
+import mongoose from "mongoose";
+import { MongoServerError } from "mongodb";
+
+function errorHandler(
+  err: unknown,
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
+  if (!(err instanceof Error))
+    return res.status(500).json({ message: "Something went wrong try again" });
+
+  let newError: { statusCode: number; message: null | string } = {
+    statusCode: 500,
+    message: "Something went wrong try again",
+  };
+  errorTypeResponse(err, newError);
+  return res.status(newError.statusCode).json({ message: err.message });
+}
+
+function errorTypeResponse(
+  err: InstanceType<typeof Error>,
+  newError: { statusCode: number; message: null | string }
+) {
+  if (err instanceof CustomAPIError) {
+    newError.statusCode = 500;
+    newError.message = err.message || "Something went wrong try again";
+    return newError;
+  }
+
+  if (err instanceof mongoose.Error.ValidationError) {
+    newError.statusCode = 400;
+    newError.message = err.message || "Something went wrong try again";
+    return newError;
+  }
+
+  if (err instanceof MongoServerError) {
+    if (err.code === 11000) {
+      newError.statusCode = 400;
+      newError.message = err.message || "Something went wrong try again";
+      return newError;
+    }
+  }
+}
+
+export { errorHandler };
