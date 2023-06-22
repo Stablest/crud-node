cdc28376afd6412dba8deb998eb6f8090e29f62e
Stablest <marlonlocci@hotmail.com>
2023-06-22 19:15:46 -0300

    refactor: getAllUsers now only gives acess to user.permission > 1

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,13 +2,20 @@ import express from "express";
 import { userModel } from "../models/User";
 import { IUser } from "../interfaces/IUser";
 import { AuthenticationError } from "../errors/authentication-error";
+import { AuthorizationError } from "../errors/authorization-error";
 
-async function getAllUsers(req: express.Request, res: express.Response) {
+async function getAllUsers(
+  req: express.Request<any, any, IUser>,
+  res: express.Response,
+  next: express.NextFunction
+) {
   try {
+    if (res.locals.user.permission <= 1)
+      throw new AuthorizationError("Not authorized to acess this route");
     const allUsers = await userModel.find();
     res.status(200).json({ users: allUsers });
-  } catch (e) {
-    res.status(500).json({ error: e });
+  } catch (err) {
+    next(err);
   }
 }
 
