b092ea7c0e8d8a1cab4294516cd2803857e26ec5
Stablest <marlonlocci@hotmail.com>
2023-06-22 19:23:58 -0300

    feat: Added auth router

---
+++ src/routers/auth.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { registerUser, loginUser } from "../controllers/auth";
+
+const authRouter = express.Router();
+
+authRouter.route("/register").post(registerUser);
+authRouter.route("/login").post(loginUser);
+
+export { authRouter };
