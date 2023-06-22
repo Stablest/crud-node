8c70e3138ef491d5c284285cc9e469e3345add44
Stablest <marlonlocci@hotmail.com>
2023-06-22 09:59:48 -0300

    feat: Added mongoose's save middleware, added createJWT method, added comparePasswod method

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,31 @@
 import mongoose from "mongoose";
-import { IUser } from "../interfaces/IUser";
+import { IUserInstance } from "../interfaces/IUser";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-const userSchema = new mongoose.Schema<IUser>({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+const UserSchema = new mongoose.Schema<IUserInstance>({
+  name: { type: String, required: [true, "Please provide a name"] },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+  },
+  password: { type: String, required: [true, "Please provide a password"] },
 });
 
-const userModel = mongoose.model("User", userSchema);
+UserSchema.pre("save", async function () {
+  const userSalt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password.toString(), userSalt);
+});
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ id: this._id }, "asasasac", { expiresIn: "30d" });
+};
+
+UserSchema.methods.comparePassword = async function (passwordReceived: string) {
+  bcrypt.compare(this.password, passwordReceived);
+};
+
+const userModel = mongoose.model("User", UserSchema);
 
 export { userModel };
