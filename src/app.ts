6e707b2a90f52a5541f5c4814d6ea3aa2edf6645
Stablest <marlonlocci@hotmail.com>
2023-07-03 23:17:24 -0300

    feat: Added organizations router

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { errorHandler } from "./middlewares/error-handler";
 import { authRouter } from "./routers/auth";
 import { authentication } from "./middlewares/authentication";
 import { taskRouter } from "./routers/tasks";
+import { organizationRouter } from "./routers/organization";
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tasks", authentication, taskRouter);
+app.use("/api/v1/organizations", authentication, organizationRouter);
 app.use(notFound);
 app.use(errorHandler);
 
