b4e5f80f3732f26d7c20e127800411ea747a0104
Stablest <marlonlocci@hotmail.com>
2023-07-06 17:25:01 -0300

    feat: Added title to Schema

--- src/models/Invites.ts
+++ src/models/Invites.ts
@@ -1,6 +1,7 @@
 import { model, Schema } from "mongoose";
 
 const inviteSchema = new Schema({
+  title: { type: String, required: true },
   org: { type: Schema.Types.ObjectId, ref: "Organization", required: true },
   message: { type: String, required: true },
   createdAt: { type: Date, expires: "7d", default: Date.now() },
