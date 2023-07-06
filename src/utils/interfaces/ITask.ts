5de67ad3bff625173a4a5a19aa65f6ac96e20ece
Stablest <marlonlocci@hotmail.com>
2023-07-06 02:30:59 -0300

    feat: Added ITaskModel, added new properties

--- src/utils/interfaces/ITask.ts
+++ src/utils/interfaces/ITask.ts
@@ -1,11 +1,23 @@
 import mongoose from "mongoose";
 
 export interface ITask {
-  name: String;
-  responsibleUsersId: mongoose.Types.ObjectId[];
-  description: String;
-  status: String;
-  label: String[];
+  name: string;
+  responsibleUsers: string[] | undefined;
+  description: string;
+  status: string;
+  label: string[];
+  org: string;
+  createdAt: Date;
 }
 
-export interface ITaskInstance extends Document, ITask {}
+export interface ITaskModel {
+  name: string;
+  responsibleUsers: mongoose.Types.ObjectId[];
+  description: string;
+  status: string;
+  label: string[];
+  org: mongoose.Types.ObjectId;
+  createdAt: Date;
+}
+
+export interface ITaskInstance extends Document, ITaskModel {}
