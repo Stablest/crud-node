a7dd8d8a28fd0cb4b7fd2f1ec84ef55d7040f98e
Stablest <marlonlocci@hotmail.com>
2023-07-06 17:29:28 -0300

    refactor: Created IOrganization, old one became IOrganizationModel

--- src/utils/interfaces/IOrganization.ts
+++ src/utils/interfaces/IOrganization.ts
@@ -1,7 +1,17 @@
 import mongoose, { Document } from "mongoose";
 import { IInviteInstance } from "./IInvite";
+import { OrganizationRemoveFromArray } from "../enums/Organization";
 
 export interface IOrganization {
+  name: string;
+  users: string[];
+  pendingUsers: string[];
+  tasks: string[];
+  invites: string[];
+  isPrivate: boolean;
+}
+
+export interface IOrganizationModel {
   name: string;
   users: IOrganizationUser[];
   pendingUsers: mongoose.Types.ObjectId[];
@@ -15,9 +25,13 @@ export interface IOrganizationUser {
   permission: Number;
 }
 
-export interface IOrganizationInstance extends Document, IOrganization {
+export interface IOrganizationInstance extends Document, IOrganizationModel {
   createAdmin: (id: string) => Promise<IOrganizationInstance>;
   checkIsAdmin: (userId: string) => boolean;
   checkIsRegistered: (userId: string) => boolean;
   checkIsTaskIncluded: (taskId: string) => boolean;
+  removeFromArray: (
+    key: OrganizationRemoveFromArray,
+    id: string
+  ) => Promise<boolean>;
 }
