794d35c8b6383d14fff616af088eaf9ce876e713
Stablest <marlonlocci@hotmail.com>
2023-07-06 02:13:43 -0300

    refactor: Removed tasks and invites routes

--- src/controllers/organization.ts
+++ src/controllers/organization.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { BadRequestError } from "../errors/bad-request";
 import { orgModel } from "../models/Organization";
 import { IOrganizationInstance } from "../utils/interfaces/IOrganization";
+import { UnknownError } from "../errors/unknown-error";
 
 async function createOrganization(
   req: Request,
@@ -38,64 +39,4 @@ async function deleteOrganization(
   }
 }
 
-async function createNewInvite(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  try {
-    const org: IOrganizationInstance = res.locals.orgDoc;
-    const { message } = req.body;
-    if (!message) throw new BadRequestError("Please provide a message");
-    const newInvite = await org.createNewInvite(message);
-    return res.status(200).json(newInvite);
-  } catch (err) {
-    next(err);
-  }
-}
-
-async function createNewTask(req: Request, res: Response, next: NextFunction) {
-  try {
-    const org: IOrganizationInstance = res.locals.orgDoc;
-    const { name, responsibleUsers, description, status, label } = req.body;
-    const task = {
-      name,
-      responsibleUsers,
-      description,
-      status,
-      label,
-    };
-    const newTask = await org.createNewTask(task);
-    return res.status(200).json(newTask);
-  } catch (err) {
-    next(err);
-  }
-}
-
-async function updateTask(req: Request, res: Response, next: NextFunction) {
-  try {
-    const org: IOrganizationInstance = res.locals.orgDoc;
-    const { taskId } = req.params;
-    if (!taskId) throw new BadRequestError("Please provide a valid task id");
-    const { name, responsibleUsers, description, status, label } = req.body;
-    const task = {
-      name,
-      responsibleUsers,
-      description,
-      status,
-      label,
-    };
-    const updatedTask = await org.updateTask(task, taskId);
-    return res.status(200).json(updatedTask);
-  } catch (err) {
-    next(err);
-  }
-}
-
-export {
-  createOrganization,
-  deleteOrganization,
-  createNewInvite,
-  createNewTask,
-  updateTask,
-};
+export { createOrganization, deleteOrganization };
