da0c04b7f168e0fa7b683cefb7d15cad4a81dbf2
Stablest <marlonlocci@hotmail.com>
2023-07-06 02:19:27 -0300

    refactor: Changed validate-org-admin folder

--- src/middlewares/organization/validate-org-admin.ts
+++ src/middlewares/organization/validate-org-admin.ts
@@ -1,10 +1,10 @@
 import { Request, Response, NextFunction } from "express";
-import { AuthenticationError } from "../errors/authentication-error";
-import { AuthorizationError } from "../errors/authorization-error";
-import { BadRequestError } from "../errors/bad-request";
-import { CustomAPIError } from "../errors/custom-api-error";
-import { orgModel } from "../models/Organization";
-import { userModel } from "../models/User";
+import { AuthenticationError } from "../../errors/authentication-error";
+import { AuthorizationError } from "../../errors/authorization-error";
+import { BadRequestError } from "../../errors/bad-request";
+import { CustomAPIError } from "../../errors/custom-api-error";
+import { orgModel } from "../../models/Organization";
+import { userModel } from "../../models/User";
 
 async function validateOrgAdmin(
   req: Request,
@@ -18,8 +18,7 @@ async function validateOrgAdmin(
     const user = await userModel.findById(res.locals.user.id);
     if (!user) throw new CustomAPIError("User not found");
     const { password } = req.body;
-    if (!password)
-      throw new BadRequestError("Please provide a password and newUser");
+    if (!password) throw new BadRequestError("Please provide a password");
     const isPasswordCorrect = await user.comparePassword(password);
     if (!isPasswordCorrect) throw new AuthenticationError("Invalid password");
     const org = await orgModel.findById(orgId);
